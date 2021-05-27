9c1c9ba7adfa828618a87f5b9926d1b3dadd3468
Danil <xdd_280@mail.ru>
2021-05-27 20:06:59 +0500

    /

--- ClockApp/ClockApp/MainPage.xaml.cs
+++ ClockApp/ClockApp/MainPage.xaml.cs
@@ -6,7 +6,10 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Input;
 using Xamarin.Forms;
+using System.Timers;
 
 namespace ClockApp
 {
@@ -20,29 +23,103 @@ namespace ClockApp
 
     }
 
+    static class Uptimer
+    {
+        static Uptimer()
+        {
+            Timer timer = new Timer(1000);
+            timer.Elapsed += async (sender, e) => await HandleTimer();
+            timer.Start();
+        }
+
+        private static Task HandleTimer()
+        {
+            Console.WriteLine("\nHandler not implemented...");
+            throw new NotImplementedException();
+
+        }
+    }
+
     public class ViewModel : BindableObject
     {
 
         public ViewModel()
         {
-            DateTime date = new DateTime();
-            date = DateTime.Now;
-
-            string sdate = date.ToString();
-
             Numbers = new ObservableCollection<char>();
 
-            Numbers.Add(sdate[11]);
-            Numbers.Add(sdate[12]);
-            Numbers.Add(sdate[13]);
-            Numbers.Add(sdate[14]);
-            Numbers.Add(sdate[15]);
-            Numbers.Add(sdate[16]);
-            Numbers.Add(sdate[17]);
-            Numbers.Add(sdate[18]);
+            CurrentTime = DateTime.Now;
+
+
+        }
+
+        private DateTime _currenttime;
+        public DateTime CurrentTime
+        {
+            get => _currenttime;
+
+            set
+            {
+                if (value == _currenttime) return;
+                _currenttime = value;
+                OnPropertyChanged(nameof(CurrentTime));
+                var DateString = _currenttime.ToString("HH:mm:ss").ToCharArray();
+                for (var i = 0; i < DateString.Length; i++)
+                {
+                    if (Numbers.Count > i)
+                    {
+                        if (Numbers[i] != DateString[i])
+                        {
+                            Numbers[i] = DateString[i];
+                        }
+                    }
+                    else
+                    {
+                        Numbers.Add(DateString[i]);
+                    }
+                }
+            }
+        }
+
+        public double FrameWidth
+        {
+            get => _pagewidth * 0.8 / 8;
+
+        }
+
+        public double FrameHeight
+        {
+            get => _pagewidth * 0.8 / 8 * 16 / 9;
+
+        }
+
+        public double MainFrameWidth
+        {
+            get => _pagewidth * 0.8;
+
+        }
+
+        public double MainFrameHeight
+        {
+            get => _pagewidth * 0.8 / 8 * 16 / 9;
+
+        }
+
+        private double _pagewidth;
+        public double PageWidth
+        {
+            get => _pagewidth;
+
+
+            set
+            {
+                if (_pagewidth == value) return;
+                _pagewidth = value;
+                OnPropertyChanged(nameof(PageWidth));
+                OnPropertyChanged(nameof(FrameWidth));
+                OnPropertyChanged(nameof(FrameHeight));
+            }
         }
 
         public ObservableCollection<char> Numbers { get; }
-
     }
 }
